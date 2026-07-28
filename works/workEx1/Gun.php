a66923494f5a28cc4fa24943f7749253129ec9a0
302A-20\cre <a@a>
2026-07-28 12:06:49 +0900

    EX1 落ちた

--- works/workEx1/Gun.php
+++ works/workEx1/Gun.php
@@ -8,15 +8,18 @@ class Gun {
     private $maxMagazine;
     // 残弾
     private $currentMagazine;
+
+    private $extendedMagazine = 0;
     // ↑フィールド============================
 
     // コンストラクタ
-    function __construct($name, $maxMagazine) {
         // 問題1
+      function __construct($name, $maxMagazine) {
         $this->name = $name;
         $this->maxMagazine = $maxMagazine;
         $this->currentMagazine = 0;
     }
+     
 
     // 現在の状態を表示
     function echoStatus() {
@@ -29,21 +32,52 @@ class Gun {
 
     // リロード
     function reload() {
-        // 問題2
+    if ($this->currentMagazine == $this->maxMagazine) {
+        echo "リロードの必要はありません\n";
+    } else {
+        $this->currentMagazine = $this->maxMagazine;
     }
+}
 
     // 発砲
     function fire() {
-        // 問題3
+    if ($this->currentMagazine == 0) {
+        echo "リロードしてください\n";
+        return;
+    }
+    $this->currentMagazine--;
+
+     echo $this->name . "を発砲しました。残弾: " . $this->currentMagazine . " 発\n";
+
+       if ($this->currentMagazine == 0) {
+        echo "リロードしてください\n";
+    }
+}
+    // 拡張マガジンを装着
+   function setExtendedMagazine($bullet) {
+
+    if (!is_int($bullet) || $bullet <= 0) {
+        echo "引数が不正です\n";
+        return;
     }
 
-    // 拡張マガジンを装着
-    function setExtendedMagazine() {
-        // 問題4
-    }
+    $this->extendedMagazine = $bullet;
+    $this->maxMagazine += $bullet;
+}
 
     // 拡張マガジンを取外し
     function unsetExtendedMagazine() {
-        // 問題4
-    }
+
+        if ($this->extendedMagazine == 0) {
+            echo "拡張マガジンは装着されていません\n";
+            return;
+        }
+
+        $this->maxMagazine -= $this->extendedMagazine;
+        $this->extendedMagazine = 0;
+
+        if ($this->currentMagazine > $this->maxMagazine) {
+            $this->reload();
+        }
+}
 }
--- works/workEx1/Main.php
+++ works/workEx1/Main.php
@@ -1,11 +1,22 @@
 <?php
 
-// Userクラスの読み込み
 require 'Gun.php';
 
-$mainWeapon = new Gun("AK47",30);
-$subWeapon = new Gun("Mosin-Nagant",5);
+$mainWeapon = new Gun("AK47", 29);
 
-// 現在の状態を表示
 $mainWeapon->echoStatus();
-$subWeapon->echoStatus();
+
+$mainWeapon->reload();
+$mainWeapon->echoStatus();
+
+$mainWeapon->fire();
+$mainWeapon->fire();
+$mainWeapon->fire();
+$mainWeapon->fire();
+
+$mainWeapon->setExtendedMagazine(10);
+$mainWeapon->reload();
+$mainWeapon->echoStatus();
+
+$mainWeapon->unsetExtendedMagazine();
+$mainWeapon->echoStatus();
